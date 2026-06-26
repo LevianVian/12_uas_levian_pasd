c9a94a22585de6375334fb8a52a66143dd6a1269
LevianVian <artalevi4@gmail.com>
2026-06-26 16:20:30 +0700

    updating

--- src/models/item.java
+++ src/models/item.java
@@ -3,41 +3,41 @@ package models;
 public class item{
     public String name;
     public String code;
-    public Double price;
+    public double price; 
     public int stock;
 
-    public item(String code, String name, double price, int stock){
+    public item(String code, String name, double price, int stock) {
         this.code = code;
         this.name = name;
         this.price = price;
         this.stock = stock;
     }
 
-    public String getCode(){
+    public String getCode() {
         return code;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public Double getPrice(){
+    public double getPrice() {
         return price;
     }
 
-    public int getStock(){
+    public int getStock() {
         return stock;
     }
 
-    public void setStock(int stock){
+    public void setStock(int stock) {
         this.stock = stock;
     }
 
     public void decreaseStock(int amount) {
-    if (this.stock >= amount) {
-        this.stock = this.stock - amount;
-    } else {
-        System.out.println("Maaf, stok barangnya tidak mencukupi");
+        if (this.stock >= amount) {
+            this.stock = this.stock - amount;
+        } else {
+            System.out.println("Maaf, stok barangnya tidak mencukupi");
         }
     }
 }
