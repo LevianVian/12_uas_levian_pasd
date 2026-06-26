ca43e3d19749e15ccd9ef2c4954eb8c1987c3184
LevianVian <artalevi4@gmail.com>
2026-06-26 14:43:46 +0700

    create a transaction models

---
+++ src/models/transaction.java
@@ -0,0 +1,47 @@
+package models;
+
+public class transaction {
+    public static int sequenceCode = 1;
+    public int tcCode;
+    public int quantity;
+    public item item;
+    public double totalPrice;
+    public double finalPrice;
+
+    
+    public int getTransactionCode(){
+        return tcCode;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public item getItem(){
+        return item;
+    }
+
+    public double getFinalPrice(){
+        return finalPrice;
+    }
+
+    public transaction(item item, int quantity, boolean isMember){
+        this.tcCode = sequenceCode++;
+        this.item = item;
+        this.quantity = quantity;
+        calculateTotal(isMember);
+    }
+
+    public void calculateTotal(boolean isMember){
+        this.totalPrice = this.item.getPrice() * this.quantity;
+        this.finalPrice = this.totalPrice;
+
+        if (this.finalPrice > 500000){
+            this.finalPrice = this.finalPrice - (this.finalPrice * 0.05);
+        }
+
+        if (isMember){
+            this.finalPrice = this.finalPrice - (this.finalPrice * 0.02);
+        }
+    }
+}
