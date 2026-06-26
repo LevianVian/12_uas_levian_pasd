600920eeca0504594112281f53a041028a444cda
LevianVian <artalevi4@gmail.com>
2026-06-26 14:16:26 +0700

    creating the base models for item

---
+++ src/models/item.java
@@ -0,0 +1,43 @@
+package models;
+
+public class item{
+    public String name;
+    public String code;
+    public Double price;
+    public int stock;
+
+    public item(String code, String name, double price, int stock){
+        this.code = code;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getCode(){
+        return code;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Double getPrice(){
+        return price;
+    }
+
+    public int getStock(){
+        return stock;
+    }
+
+    public void setStock(int stock){
+        this.stock = stock;
+    }
+
+    public void decreaseStock(int amount) {
+    if (this.stock >= amount) {
+        this.stock = this.stock - amount;
+    } else {
+        System.out.println("Maaf, stok barangnya tidak mencukupi");
+        }
+    }
+}
